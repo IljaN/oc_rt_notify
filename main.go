dda2430484d06a2be42e07642fa8726719cb737c
Ilja Neumann <ineumann@owncloud.com>
2017-10-02 23:33:05 +0200

    Simplified structure

--- main.go
+++ main.go
@@ -1,35 +1,73 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-contrib/location"
 	"github.com/gin-gonic/gin"
 	"github.com/nats-io/go-nats"
-	"ocevent/controller"
-	nr "ocevent/router"
+	"io"
+	"net/http"
+	"strings"
 )
 
+var nc *nats.Conn
+
 func main() {
-	router, named := configure()
-	n, _ := nats.Connect("nats://localhost:4222")
-	ctrl := controller.NewController(named, n)
-	named.Routes(
-		nr.C("GET", "/", "index", ctrl.Index),
-		nr.C("POST", "/events", "create_event", ctrl.Publish),
-		nr.C("GET", "/events", "event_stream", ctrl.Stream),
-	)
+	router := gin.Default()
+	nc, _ = nats.Connect("nats://localhost:4222")
 
-	router.Run()
-
-}
-
-func configure() (*gin.Engine, *nr.Named) {
-	r := gin.Default()
-	r.Use(location.New(location.Config{
+	router.Use(location.New(location.Config{
 		Scheme: "http",
 		Host:   "localhost:8080",
 	}))
 
-	r.LoadHTMLGlob("templates/*")
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", Index)
+	router.POST("/events", Publish)
+	router.GET("/events", Stream)
+	router.Run()
 
-	return r, nr.CreateNamedRouter(r)
+}
+
+func Index(c *gin.Context) {
+	url := location.Get(c)
+	url.Path = "/events"
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		"eventSourceHost": url.String(),
+	})
+}
+
+func Publish(c *gin.Context) {
+	var event struct {
+		Type string `json:"type" binding:"required"`
+		To   string `json:"to" binding:"required"`
+	}
+
+	c.BindJSON(&event)
+	nc.Publish(event.To, []byte(event.Type))
+	c.Status(http.StatusAccepted)
+}
+
+func Stream(c *gin.Context) {
+	c.Header("Content-Type", "text/event-stream")
+	c.Header("Cache-Control", "no-cache")
+	c.Header("Connection", "keep-alive")
+
+	messages := make(chan *nats.Msg, 15)
+	topics := strings.Split(c.DefaultQuery("topics", ""), ";")
+
+	for _, t := range topics {
+		nc.Subscribe(t, func(msg *nats.Msg) {
+			messages <- msg
+
+		})
+	}
+
+	c.Stream(func(w io.Writer) bool {
+		select {
+		case msg := <-messages:
+			c.SSEvent(msg.Subject, fmt.Sprintf("%s", msg.Data))
+		}
+		return true
+	})
 }
